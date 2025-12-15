56dc7e7ccfca388ab37c72675e01afe6162b1ffa
sunnatabdullaew-jpg <sunnatabdullaew@gmail.com>
2025-12-14 18:36:12 -0600

    AdminPan

--- untils/dashboard.js
+++ untils/dashboard.js
@@ -1,8 +1,30 @@
-const logOut = document.querySelector('.logout');
+const logOut = document.querySelector(".logout");
 
 const Logout = () => {
-  localStorage.removeItem('token');
-  window.location.href = '../index.html';
-}
+  localStorage.removeItem("token");
+  window.location.href = "../index.html";
+};
 
 logOut.addEventListener("click", Logout);
+
+function showToast(color, text) {
+  Toastify({
+    text: `${text}`,
+    duration: 5000, // 5 soniya
+    gravity: "top", // Yuqori qism
+    position: "right", // O'ng tomon
+    close: false,
+    stopOnFocus: true,
+    style: {
+      background: "green",
+      color: "white", // Matn rangini qora qilamiz
+      borderRadius: "5px", // Yumaloq burchaklar
+      borderLeft: `5px solid ${color}`, // Chap tomonda ko'k rangli chiziq qo'shamiz (progress bar o'rniga)
+      // boxShadow: "0 3px 10px rgba(0,0,0,0.1)", // Engil soya
+      padding: "15px",
+      fontFamily: "Open Sans",
+      boxShadow: `2px 0px 10px -3px ${color}`
+    }
+  }).showToast();
+}
+showToast("green", "Welcome Admin!");
--- untils/login.js
+++ untils/login.js
@@ -25,15 +25,41 @@ function handleSubmit(e) {
     .then((data) => {
       const token = data.token;
 
-      
       if (token) {
         localStorage.setItem("token", token);
 
-
+        
         window.location.href = "../pages/dashboard.html";
       }
-    });
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      showToast("red", "Your not admin!");
+    })
+
 
   console.log("Username:", username);
   console.log("Password:", password);
+}
+    function showToast(color, text) {
+    Toastify({
+      text: `${text}`, 
+      duration: 5000, // 5 soniya
+      gravity: "top", // Yuqori qism
+      position: "right", // O'ng tomon
+      close: false, 
+      stopOnFocus: true, 
+      style: {
+          background: `${color}`,
+          color: "white", // Matn rangini qora qilamiz
+          borderRadius: "5px", // Yumaloq burchaklar
+          borderLeft: `5px solid ${color}`, // Chap tomonda ko'k rangli chiziq qo'shamiz (progress bar o'rniga)
+          // boxShadow: "0 3px 10px rgba(0,0,0,0.1)", // Engil soya
+          padding: "15px",
+          fontFamily: "Open Sans",
+          boxShadow: `2px 0px 10px -3px ${color}`,
+          
+      },
+      
+  }).showToast();
 }
